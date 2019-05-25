06f424ae50445b3e4cdff9850f9498ff8043b6b3
Glenn Gijsberts <glenngijsberts95@gmail.com>
2019-05-26 00:31:01 +0200

    💅

---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+const colors = {
+    black: '#001319',
+    grey: '#F5F5F6',
+    greyDark: '#939B9D',
+    greyLight: '#99A1A3',
+    earth: '#00B6F0',
+}
+
+const shadow = '0 1px 2px rgba(0,19,25,0.2), 0 4px 12px rgba(0,19,25,0.1)'
+
+export { colors, shadow }
